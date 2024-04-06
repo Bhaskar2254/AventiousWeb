23ff35382b61c4b2a54270856340612f97b97172
Bhaskar2254 <80898201+Bhaskar2254@users.noreply.github.com>
2024-04-06 18:36:19 +0530

    First commit

---
+++ myscripts.js
@@ -0,0 +1,9 @@
+
+
+// var main= document.getElementById("counter");
+// window.onscroll=function(){
+//     main.counterUp({
+//             delay: 10,
+//             time: 1200
+//           });
+//       } 
